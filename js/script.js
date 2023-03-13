b11a4decd903e6cd7f2309ebd4ffc1a627fe0771
Lip-7 <zhighofficial@gmail.com>
2023-03-13 16:04:08 +0100

    minor fixes

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@ const initialBtns = document.querySelectorAll('#initialBtn button');
 const mailform = document.querySelector('#mailCheker form');
 const mailsList = ["john.smith@gmail.com","jane.doe@hotmail.com","peter.parker@yahoo.com","mary.johnson@outlook.com","kevin.nguyen@gmail.com","samantha.lee@hotmail.com","jacob.wilson@yahoo.com","amanda.brown@outlook.com","michael.chen@gmail.com","jonSnow@winterfell.com"];
 /* initial animations */
-separe(initialBtns)
+separe(initialBtns);
 /* associate main buttons to events */
 initialBtns[0].addEventListener('click',() => {
-    switchVisibility('initial','mailCheker')
+    switchVisibility('initial','mailCheker');
 })
 initialBtns[1].addEventListener('click',() => {
-    switchVisibility('initial','diceGame')
+    switchVisibility('initial','diceGame');
 })
 /* Remove the refresh on input type submit */
-preventSubmitRefresh(mailform)
+preventSubmitRefresh(mailform);
 
 
 document.getElementById('btnSubmitMail').addEventListener('click', () => {
@@ -20,12 +20,12 @@ document.getElementById('btnSubmitMail').addEventListener('click', () => {
     let result = false
     for (let i = 0; i < mailsList.length; i++){
         if (mailsList[i] == userMail){
-            result = true
+            result = true;
         }
     }
     if (result) {
-        switchVisibility('mailForm', 'mailResult')
+        switchVisibility('mailForm', 'mailResult');
     }else{
-        alert('Sorry but your mail is not in our database')
+        alert('Sorry but your mail is not in our database');
     }
-})
+});
