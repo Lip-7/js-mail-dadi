3e09657b6f4a67041de4386e33c8020165da0522
Lip-7 <zhighofficial@gmail.com>
2023-03-13 17:29:26 +0100

    fixed the dice game

--- js/script.js
+++ js/script.js
@@ -20,12 +20,12 @@ document.getElementById('btnRoll').addEventListener('click', () => {
     document.getElementById('userNumber').innerHTML = '';
     document.getElementById('pcNumber').innerHTML = '';
     document.getElementById('winnerNumber').innerHTML = '';
-    for (let i = 0; i < 2; i++) {
-        let pcwinner = true
+    let pcwinner = true
         let draw = false
         let userNumber = 0
         let pcNumber = 0
         let result = ''
+    for (let i = 0; i < 2; i++) {
         for (let j = 0; j < 10; j++) {
             if (i == 0) {
                 setTimeout(() => {
@@ -41,23 +41,23 @@ document.getElementById('btnRoll').addEventListener('click', () => {
                 }, j * 100);
             }
         }
-        setTimeout(() => {
-            if (userNumber > pcNumber) {
-                pcwinner = false
-            }else if (userNumber == pcNumber){
-                draw = true
-            }
-            /* let result = draw ? "It's a DRAW!!" : pcwinner ? "I Won!! Mhuahua" : "You won :(" */
-            if (draw){
-                result = "It's a DRAW!!";
-            }else if (pcwinner){
-                result = "I Won!! Mhuahua";
-            }else{
-                result = "You won :(";
-            }
-            console.log(result)
-            console.log(draw)
-            document.getElementById('winnerNumber').innerHTML = result
-        }, 2500)
     }
+    setTimeout(() => {
+        if (userNumber > pcNumber) {
+            pcwinner = false
+        }else if (userNumber == pcNumber){
+            draw = true
+        }
+        /* let result = draw ? "It's a DRAW!!" : pcwinner ? "I Won!! Mhuahua" : "You won :(" */
+        if (draw){
+            result = "It's a DRAW!!";
+        }else if (pcwinner){
+            result = "I Won!! Mhuahua";
+        }else{
+            result = "You won :(";
+        }
+        console.log(result)
+        console.log(draw)
+        document.getElementById('winnerNumber').innerHTML = result
+    }, 2500)
 });
